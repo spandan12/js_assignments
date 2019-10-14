20d019ce4bc2d8cc12ece01445cb35ff92bcd459
spandan12 <spandan.pyakurel.1996@gmail.com>
2019-10-14 21:11:48 +0545

    car game finished

--- CarGame/js/script.js
+++ CarGame/js/script.js
@@ -1,15 +1,30 @@
 var CARWIDTH = 70;
 var CARHEIGHT = 100;
+var COUNT = 0;
+function randomNumber(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 class GameWindow{
     constructor(){
+        this.moveInterval = null;
+        this.updateInterval = null;
+        this.obstacleInterval = null;
+        this.score = 0;
         this.width = null;
         this.height = null;
+        this.canvas = null;
         this.context = null;
         this.drawGameWindow();
-        this.drawLane(200);
-        this.drawLane(400);
-        this.playCar =  new Car(270,500,this.context);
+        this.bgTop =  0;
+        this.moveRoad();
+        this.playCar =  new playCar(1,500,this.context,false);
+        this.obstacles = [];
+        this.createObstacles();
+        // this.obstable = new obstaleCar(2,0,this.context);
+        this.updateCanvas();
     }
     
     drawGameWindow(){
@@ -17,39 +32,199 @@ class GameWindow{
         this.context = this.canvas.getContext('2d');
     }
 
-    drawLane(positionX){
-        this.context.setLineDash([5, 3]);
-        this.context.strokeStyle = 'white';
-        this.context.beginPath();
-        this.context.moveTo(positionX,0);
-        this.context.lineTo(positionX,600);
-        this.context.stroke();
+    moveRoad(){
+        var that=this;
+        this.moveInterval = setInterval(function(){
+            that.bgTop += 2;  
+            Object.assign(that.canvas.style,{
+                backgroundPosition: `0 ${that.bgTop}px`, 
+            });
+            // console.log(that.bgTop);
+        }, 50);
+    }
+
+    createObstacles(){
+        var that=this;
+        this.obstacleInterval = setInterval(function(){
+            let index = randomNumber(0,3);
+            let obstacle = new obstaleCar(index,0,that.context);
+            that.obstacles.push(obstacle);
+            // console.log(that.obstacles);
+        }, 3000);
+    }
+
+    updateScore(){
+        document.getElementById('score').innerHTML = 'Score: ' + this.score;
+    }
+
+    checkCollisions(){
+        for(let i=0; i<this.obstacles.length; i++){
+            if((this.obstacles[i].doesCollide(this.playCar))==true){
+                this.gameOver();         
+            }
+        }      
+    }
+
+
+    removeObstacles(){
+        console.log(this.obstacles.length);
+        for(let i=0; i<this.obstacles.length; i++){
+            if(((this.obstacles[i].isOutOfCanvas())==true) && ((this.obstacles[i].doesCollide(this.playCar))==false)){
+                this.obstacles.pop(this.obstacles[i]);
+                this.score +=1; 
+                // console.log('hello');      
+            }
+        } 
+
+    }
+    
+    updateAllObstacles(){
+        for(let i=0; i<this.obstacles.length; i++){
+            this.obstacles[i].updateObstacle();
+        }
+    }
+
+    updateCanvas(){
+        var that=this;
+        this.updateInterval = setInterval(function(){
+            that.context.clearRect(0, 0, canvas.width, canvas.height);
+            that.playCar.updatePlayCar();
+            that.updateAllObstacles();
+            that.checkCollisions();
+            that.removeObstacles();
+            that.updateScore();
+        }, 1);
+    }
+
+    gameOver(){
+        clearInterval(this.moveInterval);
+        clearInterval(this.updateInterval);
+        clearInterval(this.obstacleInterval);
+        document.getElementById('text').style.display = 'block';
+        document.getElementById('game-over').style.display = 'block';
     }
 }
 
 
 class Car{
-    constructor(x,y,ctx){
-        this.x = x;
+    constructor(index,y,ctx){
         this.y = y;
+        this.index = index;
+        this.x = (this.index * 200) + 70;
         this.context = ctx;
-        this.draw();
+        this.img = new Image();       
+        
     }
 
-    draw(){
-        var img = new Image();
-        img.src = './images/2.png';
-        console.log(img);
-        var that=this
-        img.onload = function () {
-            that.context.drawImage(img, that.x, that.y, CARWIDTH, CARHEIGHT);
-        };
+}
+
+class obstaleCar extends Car{
+    constructor(index,y,ctx){
+        super(index,y,ctx);
+        this.drawObstacle();
+    }
+
+
+    drawObstacle(){
+        
+        var imgRN = randomNumber(1,5);
+        this.img.src = `./images/obstacle${imgRN}.png`;
+        var that=this;
+        this.img.onload = function () {
+            that.context.drawImage(that.img, that.x, that.y, CARWIDTH, CARHEIGHT);
+        };  
+    }
+
+    move(){
+        this.y += 1;
+    }
+
+    updateObstacle(){
+        this.x = (this.index * 200) + 70;
+        this.move();
+        this.context.drawImage(this.img, this.x, this.y, CARWIDTH, CARHEIGHT);
+    }
+
+    doesCollide(car){
+        // console.log(this.y);
+       
+        if(car.index==this.index){
+            
+            if(this.y >= 400){return true;}
+            else{return false;}
+        }
+        else{return false;}
+    }
+
+    isOutOfCanvas(){
+        if(this.y >= 550){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 
 }
 
+class playCar extends Car{
+    constructor(index,y,ctx){
+        super(index,y,ctx);
+        this.drawPlaycar();
+        var that = this;
+        this.img.src = './images/playcar2.png';
+        var Interval = setInterval(function(){
+            document.onkeydown = this.onkeyStroke.bind(this);
+        }.bind(this), 1);
+    }
 
-var newGame = new GameWindow();
+    
+    drawPlaycar(){
+        var that=this;
+        this.img.onload = function () {
+            // console.log(that.x);
+            that.context.drawImage(that.img, that.x, that.y, CARWIDTH, CARHEIGHT);
+        };
+    }
+
+    updatePlayCar(){
+        this.x = (this.index * 200) + 70;
+        this.context.drawImage(this.img, this.x, this.y, CARWIDTH, CARHEIGHT);
+    }
+
+    onkeyStroke(e){
+        e = e || window.event;
+        if (e.keyCode == '37') {
+            if(this.index == 1){
+                this.index = 0;    
+            }
+            else if(this.index == 2){
+                this.index = 1;    
+            }
+            
+        }
+        else if (e.keyCode == '39') {
+            if(this.index == 1){
+                this.index = 2;    
+            }
+            else if(this.index == 0){
+                this.index = 1;   
+            }
+        }
+            
+    }
+    
+}
+
+function startGame(){
+    document.getElementById('text').style.display = 'none';
+    document.getElementById('game-over').style.display = 'none';
+    let newGame = new GameWindow();
+
+}
+
+
+
 
 
--- Flappy bird/js/script.js
+++ Flappy bird/js/script.js
@@ -1,22 +1,28 @@
+var CARWIDTH = 70;
+var CARHEIGHT = 100;
+
 class GameWindow{
-    constructor(width,height){
-        this.width = width;
-        this.height = height;
+    constructor(){
+        this.width = null;
+        this.height = null;
         this.context = null;
         this.drawGameWindow();
-        this.
-        this.playCar =  new Car(10,10,this.context);
+        this.playCar =  new Car(270,500,this.context);
     }
     
     drawGameWindow(){
-        this.canvas = document.createElement('canvas');
+        this.canvas = document.getElementById('canvas');
         this.context = this.canvas.getContext('2d');
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.canvas.style.height = this.width + 'px';
-        this.canvas.style.width  = this.height + 'px';  
     }
 
-
+    drawLane(positionX){
+        this.context.setLineDash([5, 3]);
+        this.context.strokeStyle = 'white';
+        this.context.beginPath();
+        this.context.moveTo(positionX,0);
+        this.context.lineTo(positionX,600);
+        this.context.stroke();
+    }
 }
 
 
@@ -25,14 +31,23 @@ class Car{
         this.x = x;
         this.y = y;
         this.context = ctx;
-        this.context.fillStyle = 'red';
-        this.context.fillRect(this.x, this.y, 20,20); 
+        this.draw();
+    }
+
+    draw(){
+        var img = new Image();
+        img.src = './images/2.png';
+        console.log(img);
+        var that=this
+        img.onload = function () {
+            that.context.drawImage(img, that.x, that.y, CARWIDTH, CARHEIGHT);
+        };
     }
 
 
 }
 
 
-var newGame = new GameWindow(600,650);
+var newGame = new GameWindow();
 
 
